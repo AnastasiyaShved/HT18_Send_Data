ff921a25f265de8b28c9a52161e8451b7adbbf52
AnastasiyaShved <pav159208@gmail.om>
2023-09-07 00:15:57 +0300

    project created

---
+++ HT_18_Send_Data/HT_18_Send_Data/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HT_18_Send_Data
+//
+//  Created by Apple on 7.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
