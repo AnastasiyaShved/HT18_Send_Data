3eacdce4d221a73906427a8071d933fbde24b52e
AnastasiyaShved <pav159208@gmail.om>
2023-09-07 03:13:22 +0300

    реализована передача backgroundColor на  вьюшку второго экрана

---
+++ HT_18_Send_Data/HT_18_Send_Data/Controllers/FirstVC.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  HT_18_Send_Data
+//
+//  Created by Apple on 7.09.23.
+//
+
+import UIKit
+
+protocol ColorBackgroundUpdateProtocol {
+    func colorBackgroundackgroundUpdate(color: UIColor)
+}
+
+
+class FirstVC: UIViewController {
+
+
+    @IBOutlet var viewColor: UIView!
+    
+   
+    @IBAction func ChangeBG() {
+        
+        let stor2 = UIStoryboard(name: "Main", bundle: nil)
+        guard let secondVC = stor2.instantiateViewController(withIdentifier: "SecondVC") as? SecondVC else { return }
+        secondVC.color = viewColor.backgroundColor ?? .gray
+        
+        navigationController?.pushViewController(secondVC, animated: true)
+    }
+    
+
+}
+
+extension FirstVC: ColorBackgroundUpdateProtocol {
+    func colorBackgroundackgroundUpdate(color: UIColor) {
+        viewColor.backgroundColor = .blue
+        
+
+    }
+    
+}
+
+
+
+
+
+
+
+
+//        var stor1 = UIStoryboard(name: "Main", bundle: nil)
+//        guard let firstVC = stor1.instantiateViewController(withIdentifier: "FirstVC") as? FirstVC else { return }
+//        firstVC. = .blue
---
+++ HT_18_Send_Data/HT_18_Send_Data/Controllers/SecondVC.swift
@@ -0,0 +1,37 @@
+//
+//  SecondVC.swift
+//  HT_18_Send_Data
+//
+//  Created by Apple on 7.09.23.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+    
+//MARK: - property -
+
+    @IBOutlet weak var colorVC: UIView! {
+        didSet {
+            colorVC.backgroundColor = color
+        }
+    }
+    var color: UIColor = .brown
+    
+//MARK: - actions -
+    @IBAction func doneTab(_ sender: UIButton) {
+    }
+
+}
+
+
+
+
+
+
+
+
+//MARK: - life circle -
+
+//MARK: - private funcs -
+//MARK: - novigation -
