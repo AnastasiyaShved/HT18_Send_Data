a1f3c825bec487cd80defd77d3affecb4200f190
AnastasiyaShved <pav159208@gmail.om>
2023-09-12 08:57:06 +0300

    hex color

--- HT_18_Send_Data/HT_18_Send_Data/Controllers/SecondVC.swift
+++ HT_18_Send_Data/HT_18_Send_Data/Controllers/SecondVC.swift
@@ -32,12 +32,17 @@ class SecondVC: UIViewController {
     @IBOutlet weak var hexColorTF: UITextField!
     @IBOutlet weak var opacityTF: UITextField!
     
+    var rgbRedValue = 0
+    var rgbGreenValue = 0
+    var rgbBlueValue = 0
+    var opacity: CGFloat = 100
     
 //MARK: - life circle -
     override func viewDidLoad() {
         super.viewDidLoad()
         colorVC.layer.borderWidth = 1
         colorVC.layer.borderColor = UIColor.darkGray.cgColor
+        convertToHex()
     }
     
 //MARK: - actions -
@@ -52,15 +57,24 @@ class SecondVC: UIViewController {
     }
     @IBAction func redSlider(_ sender: UISlider) {
         redTF.text = formatterForSlider(sender.value)
+        rgbRedValue = Int(sender.value)
+        convertToHex()
+        
     }
     @IBAction func greenSlider(_ sender: UISlider) {
         greenTF.text = formatterForSlider(sender.value)
+        rgbGreenValue = Int(sender.value)
+        convertToHex()
     }
     @IBAction func blueSlider(_ sender: UISlider) {
         blueTF.text = formatterForSlider(sender.value)
+        rgbBlueValue = Int(sender.value)
+        convertToHex()
     }
     @IBAction func opacitySlider(_ sender: UISlider) {
-        opacityTF.text = formatterForSlider(sender.value)+"%"
+        opacityTF.text = formatterForSlider(sender.value) + "%"
+        opacity = CGFloat(sender.value)
+        convertToHex()
     }
     
     @IBAction func redAction(_ sender: UITextField) {
@@ -77,6 +91,13 @@ class SecondVC: UIViewController {
         opacitySl.setValue(formaterForTexFild(sender.text), animated: true)
     }
     
+    @IBAction func hexColorAction(_ sender: UITextField) {
+
+
+    }
+    
+    
+    
     //MARK: - private funcs -
     private func formatterForSlider (_ value: Float) -> String {
         return String(lround(Double(value)))
@@ -85,7 +106,28 @@ class SecondVC: UIViewController {
     private func formaterForTexFild (_ text: String?) -> Float {
         return Float(String(text ?? "")) ?? 0
     }
+    private func convertToHex() {
+        hexColorTF.text = String(format:"%02X", Int(rgbRedValue)) + String(format:"%02X", Int(rgbGreenValue)) + String(format:"%02X", Int(rgbBlueValue))
+        let color = UIColor(red: rgbRedValue, green: rgbGreenValue, blue: rgbBlueValue).withAlphaComponent(CGFloat(opacity / 100))
+        colorVC.backgroundColor = color
+    }
+
+
 }
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+}
+}
+
+
+
+
 
 
 
